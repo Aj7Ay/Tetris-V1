8a1bb89538e9a1b097a92e04f07303717d0b2b94
Aj7ay <ajayyegireddi007@gmail.com>
2023-10-26 09:09:50 +0530

    updated install.sh file

--- Jenkins-terraform/install_jenkins.sh
+++ Jenkins-terraform/install_jenkins.sh
@@ -28,6 +28,7 @@ sudo apt-get update
 sudo apt-get install trivy -y
 
 #install terraform
+sudo apt install wget -y
 wget -O- https://apt.releases.hashicorp.com/gpg | sudo gpg --dearmor -o /usr/share/keyrings/hashicorp-archive-keyring.gpg
 echo "deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/hashicorp.list
 sudo apt update && sudo apt install terraform
