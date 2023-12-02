3c35eb52ea547492bd6a9d1ccd17d3615e037434
prabinrajbanshi <rajbprabin@gmail.com>
2023-12-02 06:00:35 +0000

    changed provider location

--- Eks-terraform/backend.tf
+++ Eks-terraform/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
-    bucket = "ajay-mrcloudbook777" # Replace with your actual S3 bucket name
-    key    = "EKS/terraform.tfstate"
-    region = "ap-south-1"
+    bucket = "ratmdatastore" # Replace with your actual S3 bucket name
+    key    = "Jenkins/terraform.tfstate"
+    region = "ap-southeast-2"
   }
 }
