8d83ccf2f194c4520d110c588a4e2af5bf0497e8
prabinrajbanshi <rajbprabin@gmail.com>
2023-12-06 03:32:28 +0000

    changed lcoation to southeast
    Signed-off-by: prabinrajbanshi <rajbprabin@gmail.com>

--- Jenkins-terraform/backend.tf
+++ Jenkins-terraform/backend.tf
@@ -2,6 +2,6 @@ terraform {
   backend "s3" {
     bucket = "ratmdatastore" # Replace with your actual S3 bucket name
     key    = "Jenkins/terraform.tfstate"
-    region = "ap-southeast-2"
+    region = "ap-southeast-1"
   }
 }
