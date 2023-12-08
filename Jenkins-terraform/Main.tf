f8e0ddb81e3f4a07654b948abbd3f1fdef5c3a5c
prabinrajbanshi <rajbprabin@gmail.com>
2023-12-08 07:44:22 +0000

    modified the key pairs

--- Jenkins-terraform/Main.tf
+++ Jenkins-terraform/Main.tf
@@ -61,7 +61,7 @@ resource "aws_security_group" "Jenkins-sg" {
 resource "aws_instance" "web" {
   ami                    = "ami-0df4b2961410d4cff"
   instance_type          = "t2.medium"
-  key_name               = "r@tm"
+  key_name               = "purplehaze"
   vpc_security_group_ids = [aws_security_group.Jenkins-sg.id]
   user_data              = templatefile("./install_jenkins.sh", {})
   iam_instance_profile   = aws_iam_instance_profile.example_profile.name
