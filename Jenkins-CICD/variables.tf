86ec1bbb78262bd6a31e54ed597352e8af94cb2f
yourfavengineer <kendall8584@yahoo.com>
2024-01-30 15:31:36 -0800

    cofnigurations

--- Jenkins-CICD/variables.tf
+++ Jenkins-CICD/variables.tf
@@ -1,18 +1,18 @@
 variable "aws_region" {
   description = "The AWS region to create things in."
-  default     = "ap-southeast-2"
+  default     = "us-west-2"
 }
 variable "key_name" {
   description = " SSH keys to connect to ec2 instance"
-  default     = "purplehaze"
+  default     = "tetriskey"
 }
 variable "instance_type" {
   description = "instance type for ec2"
-  default     = "t2.medium"
+  default     = "t2.large"
 }
 variable "ami_id" {
   description = "AMI for Ubuntu Ec2 instance"
-  default     = "ami-0df4b2961410d4cff"
+  default     = "ami-008fe2fc65df48dac"
 }
 variable "bucket_name" {
   description = "The name of the S3 bucket to create"
--- Jenkins-terraform/provider.tf
+++ Jenkins-terraform/provider.tf
@@ -9,5 +9,5 @@ terraform {
 
 # Configure the AWS Provider
 provider "aws" {
-  region = "ap-southeast-2"
+  region = "us-west-2"
 }
