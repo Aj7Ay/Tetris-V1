83ab57077702b8e0c531711fc24ee67d8062e342
Gaddam Sudeep Kumar Reddy <156163418+Sudeepreddygaddam@users.noreply.github.com>
2024-10-08 17:49:21 +0530

    Update backend.tf

--- Jenkins-terraform/backend.tf
+++ Jenkins-terraform/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "ajay-mrcloudbook777" # Replace with your actual S3 bucket name
+    bucket = "nagamnaikanta" # Replace with your actual S3 bucket name
     key    = "Jenkins/terraform.tfstate"
     region = "ap-south-1"
   }
