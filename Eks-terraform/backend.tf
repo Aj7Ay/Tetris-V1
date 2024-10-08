cc92218757da43ea2a0659cbae80f5f4b87dc0b2
Gaddam Sudeep Kumar Reddy <156163418+Sudeepreddygaddam@users.noreply.github.com>
2024-10-08 17:48:57 +0530

    Update backend.tf

--- Eks-terraform/backend.tf
+++ Eks-terraform/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "ratmdatastore" # Replace with your actual S3 bucket name
+    bucket = "nagamnaikanta" # Replace with your actual S3 bucket name
     key    = "eks/terraform.tfstate"
     region = "ap-south-1"
   }
